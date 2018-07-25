e9970d54f75a49e3a208753c8e4608a84819ddc5
JMXXII <jeysonmmurillo@gmail.com>
2018-07-24 22:10:51 -0400

    1 challange

---
+++ stringIncrementer.js
@@ -0,0 +1,27 @@
+// Your job is to write a function which increments a string, to create a new string. If the string already ends with a number, the number should be incremented by 1. If the string does not end with a number the number 1 should be appended to the new string.
+
+// Examples:
+
+// foo -> foo1
+
+// foobar23 -> foobar24
+
+// foo0042 -> foo0043
+
+// foo9 -> foo10
+
+// foo099 -> foo100
+
+// Attention: If the number has leading zeros the amount of digits should be considered.
+
+
+function incrementString (strng) {
+  var string = strng.replace(/[0-9]/g, '');
+  var len = strng.length - string.length;
+  var str = strng.slice(string.length) || "0";
+    str = (parseInt(str) + 1).toString();
+  while (str.length < len) {
+    str = "0" + str;
+  } 
+    return string.concat(str);
+}
