3f45178b774272994d48947fca5691992ae86d8f
jmxxii <37158064+jmxxii@users.noreply.github.com>
2018-09-05 18:25:31 -0400

    Make the dead fish swim

---
+++ deadfishSwim.js
@@ -0,0 +1,31 @@
+/*
+Write a simple parser that will parse and run Deadfish.
+
+Deadfish has 4 commands, each 1 character long:
+
+i increments the value (initially 0)
+d decrements the value
+s squares the value
+o outputs the value into the return array
+Invalid characters should be ignored.
+
+parse("iiisdoso") => [ 8, 64 ]
+*/
+
+function parse( data ){
+let count = 0;
+let result = [];
+
+for(let i = 0; i < data.length; i++){
+    if(data[i] === "i"){
+      count++
+    } else if(data[i] === "d"){
+      count--
+    } else if(data[i] === "s"){
+      count = counter*counter
+    } else if(data[i] === "o"){
+      result.push(counter)
+    }
+  }
+  return result
+}
